bbc1e96bbed140d4ab54bb7bd91412fda5781deb
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-08-05 08:12:04 +0100

    Added FontAwesomeIcon

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,10 +1,14 @@
+import {faHeart} from "@fortAwesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 // import Image from "next/image";
 
 export default function JobRow() {
     return (
         <>
-         <div className="bg-white p-4 rounded-lg shadow-sm">
-            <div className="absolute top-0 right-0">Heart</div>
+         <div className="bg-white p-4 rounded-lg shadow-sm relative">
+            <div className="absolute cursor-pointer top-4 right-4">
+                <FontAwesomeIcon className="size-4 text-gray-300" icon={faHeart} />
+            </div>
             <div className="flex grow gap-4">
                 <div className="content-center">
                     <img className="size-12" src="https://www.logo.wine/a/logo/Spotify/Spotify-Icon-Logo.wine.svg" alt="spotify" />
