4797eafb10103680c9e3d13a0f7a03e3dd240408
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2024-09-27 12:08:06 +0100

    Resolving dependency issue

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,5 +1,5 @@
-import {faHeart} from "@fortAwesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+// import {faHeart} from "@fortAwesome/free-solid-svg-icons";
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 // import Image from "next/image";
 
 export default function JobRow() {
@@ -7,7 +7,7 @@ export default function JobRow() {
         <>
          <div className="bg-white p-4 rounded-lg shadow-sm relative">
             <div className="absolute cursor-pointer top-4 right-4">
-                <FontAwesomeIcon className="size-4 text-gray-300" icon={faHeart} />
+                {/* <FontAwesomeIcon className="size-4 text-gray-300" icon={faHeart} /> */}
             </div>
             <div className="flex grow gap-4">
                 <div className="content-center">
