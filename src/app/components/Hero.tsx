8a101e759b8c0d361f563294027ea17418eb4205
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2025-04-14 11:51:33 +0100

    Homepage - Imported Icon

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
             {/* <p className="text-center text-gray-600 mt-2">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Et tempora reprehenderit consequatur adipisci assumenda? rum aliquam cum.
             </p> */}
-            <form className="flex gap-2 mt-4 max-w-xl mx-auto">
+            <form className="flex gap-2 mt-4 max-w-md mx-auto">
                 <input type="search" className="border border:gray-400 w-full py-2 px-3 rounded-md" placeholder="Search phrase..." />
                 <button className="bg-blue-600 text-white py-2 px-4 rounded-md">Search</button>
             </form>
